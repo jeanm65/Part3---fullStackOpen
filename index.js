4198a8c0b09a4b183c920f69165af048053077fb
jinh013 <jm.randrianant65@gmail.com>
2023-06-02 21:39:51 +0300

    use of morgan

--- index.js
+++ index.js
@@ -1,101 +1,108 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
+const morgan = require("morgan");
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
 let persons = [
-    { 
-      "id": 1,
-      "name": "Artos Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendick", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Artos Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+];
 
 const noteDate = new Date();
 
 const newId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1 
-  }
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.get('/', (request, response) => {
-    response.send('welcome world!');
-  })
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
-})
-
-app.get('/info', (request, response) => {
-  const personsLength = Object.keys(persons).length;
-  response.send(`phonebook has info of ${personsLength} peoples <br>  ${noteDate}`);
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(person => person.id === id);
-  console.log('person', person);
-  if(person){
-    response.json(person);
-  }else {
-    response.status(404).end();
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.filter(person => person.id !== id);
-    response.status(204).end();
-})
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-
-    const content = ({
-      name:body.name,
-      number: body.number
-    })
-
-    const findRepeatedName = persons.find(person => body.name === person.name);
-
-    if(!content ){
-      return response.status(400).json({ 
-        error: 'content missing' 
-    });}
-    if(findRepeatedName){
-      response.send('the name already exists in the phonebook');
-    }
-
-    const person = {
-      content: content,
-      id: newId
-    }
-    
-    response.json(person);
+app.get("/", (request, response) => {
+  response.send("welcome world!");
 });
 
-//using morgan
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const personsLength = Object.keys(persons).length;
+  response.send(
+    `phonebook has info of ${personsLength} peoples <br>  ${noteDate}`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  console.log("person", person);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  const content = {
+    name: body.name,
+    number: body.number,
+  };
+
+  const findRepeatedName = persons.find((person) => body.name === person.name);
+
+  if (!content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  if (findRepeatedName) {
+    response.send("the name already exists in the phonebook");
+  }
+
+  const person = {
+    content: content,
+    id: newId,
+  };
+
+  response.json(person);
+
+  morgan.token("body", (request) => JSON.stringify(request.body));
+});
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
-  console.log('server running on PORT', PORT);
-})
+  console.log("server running on PORT", PORT);
+});
