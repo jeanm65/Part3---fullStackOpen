11538acdd47529eef7cfa7551e506e82cd60fd3c
jinh013 <jm.randrianant65@gmail.com>
2023-06-11 06:54:06 +0300

    exercise 3.9 refactored

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const morgan = require("morgan");
 
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
 
 app.use(express.static("build"));
+
+app.use(cors());
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
